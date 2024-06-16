3d98939b59ce75c2b35fa0251348b84f0dca99ba
SneakyDonut <jairampranav@gmail.com>
2024-06-16 13:12:52 +0200

    brev2

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="md:mt-[170px] flex flex-col items-center mt-[150px]">
         <div className="flex flex-col items-center gap-2 md:gap-4 md:w-[800px]">
           <h1 className="md:text-4xl text-2xl font-extrabold text-center px-5 md:px-0">
-            SOLVE YOUR <span className="text-[#E51C1B]">AUTOMOTIVE ISSUES</span> WITH MESAS <span className="text-[#E51C1B]">FINEST AUTO REPAIR SHOP</span>
+            SOLVE YOUR <span className="text-[#E51C1B]">AUTOMOTIVE ISSUES</span> WITH MESA&apos;S <span className="text-[#E51C1B]">FINEST AUTO REPAIR SHOP</span>
           </h1>
           <p className="md:text-sm text-[10px] text-gray-500 md:mb-0 mb-10"><i>Expert auto repair in Mesa with affordable, guaranteed service; call now.</i></p>
         </div>
