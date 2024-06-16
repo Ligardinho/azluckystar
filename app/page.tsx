43544bffbbabd72ba5f296fc3c9a5a26f2d5ea1d
SneakyDonut <jairampranav@gmail.com>
2024-06-16 13:59:32 +0200

    commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <h1 className="md:text-4xl text-2xl font-extrabold text-center px-5 md:px-0">
             SOLVE YOUR <span className="text-[#E51C1B]">AUTOMOTIVE ISSUES</span> WITH MESA&apos;S <span className="text-[#E51C1B]">FINEST AUTO REPAIR SHOP</span>
           </h1>
-          <p className="md:text-sm text-[10px] text-gray-500 md:mb-0 mb-10"><i>Expert auto repair in Mesa with affordable, guaranteed service; call now.</i></p>
+          <p className="md:text-sm text-[10px] text-gray-500 md:mb-0 mb-10 text-center"><i>Expert auto repair in Mesa with affordable, guaranteed service; call now.</i></p>
         </div>
         <Testimonial/>
         <CarouselDemo/>
